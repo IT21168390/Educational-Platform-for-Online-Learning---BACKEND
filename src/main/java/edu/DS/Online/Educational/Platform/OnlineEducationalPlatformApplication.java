b78adb49d75ed11a8a3af4865e502977e05004ff
IT21168390 <it21168390@my.sliit.lk>
2024-04-29 20:46:32 +0530

    Initial commit

---
+++ src/main/java/edu/DS/Online/Educational/Platform/OnlineEducationalPlatformApplication.java
@@ -0,0 +1,13 @@
+package edu.DS.Online.Educational.Platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineEducationalPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineEducationalPlatformApplication.class, args);
+	}
+
+}
